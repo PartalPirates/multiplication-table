bc2fc3f2e087c252bd08cf14bb9ad182f1a935ce
PartalPirates <80158791+PartalPirates@users.noreply.github.com>
2021-07-03 14:15:38 +0200

    Add files via upload

---
+++ eins mal eins Tabelle ( für Github ) .py
@@ -0,0 +1,15 @@
+import string
+import copy
+Tabelle=[1 , 2, 3 , 4 , 5 , 6 , 7 , 8 , 9 ]
+Nummer=copy.copy(Tabelle)
+print(Tabelle)
+for i in Tabelle:
+    print("Die erste Tabelle ist : " , i )
+    for x in Nummer:
+        print("die Nummer ist",x)
+        #print(i*x)
+        A=list(map(lambda i:i*x , Tabelle))
+        print(A)
+    
+
+
