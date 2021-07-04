76f17c66a9432e211155b98d3b4bd9c134967306
PartalPirates <80158791+PartalPirates@users.noreply.github.com>
2021-07-04 13:42:54 +0200

    Update multiplication_table.py
    I made the code shorter

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,15 +1,6 @@
-import string
-import copy
-Tabelle=[1 , 2, 3 , 4 , 5 , 6 , 7 , 8 , 9 ]
-Nummer=copy.copy(Tabelle)
-print(Tabelle)
-for i in Tabelle:
-    print("Die erste Tabelle ist : " , i )
-    for x in Nummer:
-        print("die Nummer ist",x)
-        #print(i*x)
-        A=list(map(lambda i:i*x , Tabelle))
-        print(A)
-    
+for i in range(10):
+    print("_______________________________")
+    for x in range(10):
+        print(f"Die {i} Reihe:\n","%d*%d=%d" % (i,x,i*x))
 
 
